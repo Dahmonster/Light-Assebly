00a79d5337b19e43ca7d2c9caf85b89ef3af3abc
Dahmonster <haezhed01@gmail.com>
2026-04-08 19:28:23 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -123,7 +123,7 @@ async function seedData() {
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('../public'));
+app.use(express.static(path.join(__dirname, '..')));
 
 // Routes
 app.post('/api/auth/login', (req, res) => {
