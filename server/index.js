40c68da8e2f8de7f2cd8f8ff7857683153e1336c
Dahmonster <haezhed01@gmail.com>
2026-04-13 22:19:39 +0100

    Add multer for handling file uploads in memory

--- server/index.js
+++ server/index.js
@@ -131,6 +131,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
