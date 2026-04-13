d6ffca53abea1f6ba83727f966dba6e2208297c2
Dahmonster <haezhed01@gmail.com>
2026-04-13 21:29:46 +0100

    Update and rename cloundinary.js to cloudinary.js

--- cloundinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: 'dgajz9htt',
-  api_key: '639156769547713',
-  api_secret: 'HNBr2s9OMPK3bd3bTUI3tjPjyOM'
-});
-
-export default cloudinary;
