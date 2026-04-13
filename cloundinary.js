c8b3807d7c7bb7b6f083e119187f7aaa55030697
Dahmonster <haezhed01@gmail.com>
2026-04-13 21:06:16 +0100

    Create cloundinary.js

---
+++ cloundinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'dgajz9htt',
+  api_key: '639156769547713',
+  api_secret: 'HNBr2s9OMPK3bd3bTUI3tjPjyOM'
+});
+
+export default cloudinary;
